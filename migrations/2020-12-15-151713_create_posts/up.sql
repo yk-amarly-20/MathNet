b7061c69c0242758059f775e50259d041d8c26f1
yk-amarly-20 <y.k.83600260@icloud.com>
2020-12-16 01:46:44 +0900

    add: migrations

---
+++ migrations/2020-12-15-151713_create_posts/up.sql
@@ -0,0 +1,21 @@
+-- Your SQL goes here
+CREATE TABLE books
+(
+    book_id INTEGER NOT NULL PRIMARY KEY,
+    title VARCHAR NOT NULL
+);
+
+CREATE TABLE posts
+(
+    post_id INTEGER NOT NULL PRIMARY KEY,
+    book_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    page INTEGER NOT NULL,
+    "text" TEXT
+);
+
+CREATE TABLE users
+(
+    user_id INTEGER NOT NULL PRIMARY KEY,
+    email_adress TEXT NOT NULL
+);
