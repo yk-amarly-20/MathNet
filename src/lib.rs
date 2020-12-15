884337c5d2ff934ff7622dd9421f300ca470b33a
yk-amarly-20 <y.k.83600260@icloud.com>
2020-12-16 01:46:24 +0900

    fix: manipulate database

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,39 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+pub mod models;
+pub mod schema;
+
+use self::models::posts::NewPost;
+use diesel::prelude::*;
+use diesel::sqlite::SqliteConnection;
+use dotenv::dotenv;
+use std::env;
+
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_SQL").expect("URL not defined");
+    SqliteConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
+
+pub fn create_posts(conn: &SqliteConnection, book_id: i32, user_id: i32,
+                    page: i32, text: &str) -> usize {
+
+    use crate::schema::posts;
+    let new_post = NewPost {book_id, user_id, page, text};
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .execute(conn)
+        .expect("Error saving new posts")
+}
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -15,5 +48,5 @@ extern {
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, math-net!");
+    unsafe{alert("Hello, math-net!");}
 }
