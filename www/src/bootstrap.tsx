d235e5a75f75e73d540810bea0916a34ce120c5a
yk-amarly-20 <y.k.83600260@icloud.com>
2020-12-21 00:00:41 +0900

    remove

--- www/src/bootstrap.tsx
+++
@@ -1,3 +0,0 @@
-// エントリーポイント。wasm を import するモジュールは非同期で import する必要があるので、
-// こうすることで、ソース内のどこでも wasm を import することができるようになる。
-import("./index").catch(e => console.error("Error importing `bundle.js`:", e));
