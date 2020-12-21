17faf884162e036014f056a66445a9328b05f424
yk-amarly-20 <y.k.83600260@icloud.com>
2020-12-22 02:09:31 +0900

    add core Book

---
+++ math_net_core/src/book.rs
@@ -0,0 +1,12 @@
+use serde::{Deserialize, Serialize};
+#[derive(Debug, Default, Serialize, Deserialize, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+#[serde(rename_all = "snake_case")]
+pub struct BookId(pub i32);
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "snake_case")]
+pub struct Book {
+    pub book_id: BookId,
+    pub title: String,
+    pub num_posts: i32
+}
