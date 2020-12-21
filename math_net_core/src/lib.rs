f4fc878690ee78769c7a502b92f1a61131d86302
yk-amarly-20 <y.k.83600260@icloud.com>
2020-12-22 02:09:52 +0900

    add core

--- math_net_core/src/lib.rs
+++ math_net_core/src/lib.rs
@@ -1,1 +1,18 @@
-extern crate serde;
+/*
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Debug, Default, Serialize, Deserialize, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+#[serde(rename_all = "snake_case")]
+pub struct BookId(pub i32);
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "snake_case")]
+pub struct Book {
+    pub book_id: BookId,
+    pub title: String,
+    pub num_posts: i32
+}
+*/
+
+pub mod book;
