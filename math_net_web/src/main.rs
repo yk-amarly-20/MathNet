dee85811dda09e33de28f82ad01f290c2a1973ff
yk-amarly-20 <y.k.83600260@icloud.com>
2020-12-27 14:08:48 +0900

    add: handlers

--- math_net_web/src/main.rs
+++ math_net_web/src/main.rs
@@ -3,14 +3,28 @@
 
 #[macro_use]
 extern crate rocket;
+extern crate rocket_contrib;
+extern crate serde;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello World"
+use serde::Serialize;
+
+use rocket_contrib::templates::Template;
+
+#[get("/hello/<name>")]
+fn index(name: String) -> Template {
+    #[derive(Serialize)]
+    struct Context {
+        name: String
+    }
+
+    let context = Context {name: name};
+
+    Template::render("base", &context)
 }
 
 fn main() {
     rocket::ignite()
         .mount("/", routes![index])
+        .attach(Template::fairing())
         .launch();
 }
